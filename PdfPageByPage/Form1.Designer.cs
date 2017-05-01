4c8543af1f33213c88ffbf4b86cb55716f04a1a4
Robin Portigliatti <robin.portigliatti@gmail.com>
2017-05-01 13:11:11 +0200

    Issue #2 solved : maximise window is no longer possible

--- PdfPageByPage/Form1.Designer.cs
+++ PdfPageByPage/Form1.Designer.cs
@@ -109,6 +109,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(512, 388);
             this.MinimumSize = new System.Drawing.Size(512, 388);
             this.Name = "PdfSplitter";
